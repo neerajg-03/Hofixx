86be32071d4e63b1a479eb500b0bd8f08b96918a
neerajg-03 <gupta.neeraj9000@gmail.com>
2025-10-28 02:42:00 +0530

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -3,6 +3,9 @@ from flask_bcrypt import Bcrypt
 from flask_socketio import SocketIO
 from mongoengine import connect
 import os
+import dns.resolver
+dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers = ["8.8.8.8", "1.1.1.1"]
 
 jwt = JWTManager()
 bcrypt = Bcrypt()
